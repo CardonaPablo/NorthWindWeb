c58d5e5f02817591e522bb8bba28a26ef30cb56b
Pablo Cardona <pablocardona@jarabesoft.com>
2019-11-21 00:26:59 -0600

    Todo jala excepto POST

--- NorthWindWeb/Pages/Suppliers.cshtml.cs
+++ NorthWindWeb/Pages/Suppliers.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NorthWindEntitiesLib;
 using NorthwindContextLib;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace WebApplication1.Pages
@@ -14,6 +15,7 @@ namespace WebApplication1.Pages
     {
         public IEnumerable<string> Suppliers { get; set; }
         private Northwind db;
+        [BindProperty] public Supplier Supplier { get; set; }
         public SuppliersModel(Northwind injectedContext)
         {
             db = injectedContext;
@@ -22,6 +24,28 @@ namespace WebApplication1.Pages
         {
             ViewData["Title"] = "NorthWind Website - Suppliers";
             Suppliers = db.Suppliers.Select(s => s.CompanyName).ToArray();
+        }
+
+        [HttpPost]
+        public IActionResult onPost()
+        {
+            try
+            {
+            if (ModelState.IsValid)
+                {
+                    db.Suppliers.Add(Supplier);
+                    db.SaveChanges();
+                }
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+            }
+
+            return RedirectToPage("/suppliers");
         }
     }
 }
